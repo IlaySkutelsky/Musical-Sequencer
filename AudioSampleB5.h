5be0094f03cdd7cb5148f3934b08130c3f664abc
unknown <dogme84@gmail.com>
2022-05-12 12:05:46 +0300

    added dir

--- AudioSampleB5.h
+++
@@ -1,3 +0,0 @@
-// Audio data converted from WAV file by wav2sketch
-
-extern const unsigned int AudioSampleB5[16897];
--- AudioSampleC5.h
+++
@@ -1,3 +0,0 @@
-// Audio data converted from WAV file by wav2sketch
-
-extern const unsigned int AudioSampleC5[3361];
